181c5a87cb01a942a0ae28adb0304214d069e718
Edward Pratt <41444303+Edward-Pratt@users.noreply.github.com>
2024-10-15 14:17:17 +0100

    Added doc comments to Job Class

--- Job.java
+++ Job.java
@@ -1,14 +1,24 @@
-public class Job implements Runnable{
-  private int jobNumberl
+public class Job implements Runnable {
+  private int jobNumber;
 
-  Job (int) jobNumber{
+  // Constructor to initialize the job number
+  Job(int jobNumber) {
     this.jobNumber = jobNumber;
   }
-  public void run () {
-    System.out.println("Job:" + jobNumber + "is being processed by thread : " + Thread.currentThread().getName());
-  try {
-    Thread.sleep(1000);
-  } catch (InterruptedException e){}
-  System.out.println("Job: " + jobNumber + " is ending in thread : " + Thread.currentThread().getName());
+
+  // The run method contains the code that constitutes the task
+  public void run() {
+    // Print the start of the job processing
+    System.out.println("Job:" + jobNumber + " is being processed by thread : " + Thread.currentThread().getName());
+
+    try {
+      // Simulate job processing time
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      // Handle the interrupted exception
+    }
+
+    // Print the end of the job processing
+    System.out.println("Job: " + jobNumber + " is ending in thread : " + Thread.currentThread().getName());
   }
 }
